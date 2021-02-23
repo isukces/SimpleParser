291ec5b149fcff36c48ed735c952a4c201478f6a
Piotr Stęclik <piotr.steclik@isukces.com>
2021-02-23 13:26:17 +0100

    adds WhitecharsTokenizer

---
+++ app/SimpleParser/iSukces.Parsers.Tests/WhitecharsTokenizerTests.cs
@@ -0,0 +1,47 @@
+﻿using iSukces.Parsers.TokenParsers;
+using Xunit;
+
+namespace SimpleParser.Tests
+{
+    public class WhitecharsTokenizerTests
+    {
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(" \n ")]
+        [Theory]
+        public void T01_Should_parse_zero_or_more(string text)
+        {
+            var t = WhitecharsTokenizer.ZeroOrMoreWhitechars;
+            var c = t.Parse(text);
+            Assert.NotNull(c);
+            Assert.Equal(text.Length - text.TrimStart().Length, c.StringLength);
+            c = t.Parse(text + "XYZ");
+            Assert.NotNull(c);
+            Assert.Equal(text.Length - text.TrimStart().Length, c.StringLength);
+        }
+
+        [InlineData(" ")]
+        [InlineData(" \n ")]
+        [Theory]
+        public void T02_Should_parse_one_or_more(string text)
+        {
+            var t = WhitecharsTokenizer.OneOrMoreWhitechars;
+            var c = t.Parse(text);
+            Assert.NotNull(c);
+            Assert.Equal(text.Length - text.TrimStart().Length, c.StringLength);
+            c = t.Parse(text + "XYZ");
+            Assert.NotNull(c);
+            Assert.Equal(text.Length - text.TrimStart().Length, c.StringLength);
+        }
+
+        [Fact]
+        public void T03_Should_not_parse_one_or_more()
+        {
+            var t = WhitecharsTokenizer.OneOrMoreWhitechars;
+            var c = t.Parse("");
+            Assert.Null(c);
+            c = t.Parse("XYZ");
+            Assert.Null(c);
+        }
+    }
+}
---
+++ app/SimpleParser/iSukces.Parsers/TokenParsers/WhitecharsTokenizer.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace iSukces.Parsers.TokenParsers
+{
+    public sealed class WhitecharsTokenizer : ValueTokenizer
+    {
+        private WhitecharsTokenizer(bool mustHaveAtLeastOne)
+        {
+            _mustHaveAtLeastOne = mustHaveAtLeastOne;
+        }
+
+
+        public override TokenCandidate Parse(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return CreateTokenCandidate(0);
+            for (var i = 0; i < text.Length; i++)
+            {
+                if (char.IsWhiteSpace(text[i]))
+                    continue;
+                return CreateTokenCandidate(i);
+            }
+
+            return CreateTokenCandidate(text.Length);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private TokenCandidate CreateTokenCandidate(int length)
+        {
+            if (_mustHaveAtLeastOne && length == 0)
+                return null;
+            return new TokenCandidate(null, length, length == 0 ? 0 : int.MaxValue);
+        }
+
+        public static WhitecharsTokenizer ZeroOrMoreWhitechars => InstanceHolder.MyZeroOrMoreWhitechars;
+        public static WhitecharsTokenizer OneOrMoreWhitechars  => InstanceHolder.MyOneOrMoreWhitechars;
+        private readonly bool _mustHaveAtLeastOne;
+
+        private sealed class InstanceHolder
+        {
+            public static readonly WhitecharsTokenizer MyZeroOrMoreWhitechars = new WhitecharsTokenizer(false);
+            public static readonly WhitecharsTokenizer MyOneOrMoreWhitechars = new WhitecharsTokenizer(true);
+        }
+    }
+}
