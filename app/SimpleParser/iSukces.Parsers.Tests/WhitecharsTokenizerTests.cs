04c0e569e25859bc81dbfefbb03b4a933ceccb7c
Piotr Stęclik <piotr.steclik@isukces.com>
2021-02-23 13:47:54 +0100

    extends WhitecharsTokenizer

--- app/SimpleParser/iSukces.Parsers.Tests/WhitecharsTokenizerTests.cs
+++ app/SimpleParser/iSukces.Parsers.Tests/WhitecharsTokenizerTests.cs
@@ -11,7 +11,7 @@ namespace SimpleParser.Tests
         [Theory]
         public void T01_Should_parse_zero_or_more(string text)
         {
-            var t = WhitecharsTokenizer.ZeroOrMoreWhitechars;
+            var t = WhitecharsTokenizer.ZeroOrMore;
             var c = t.Parse(text);
             Assert.NotNull(c);
             Assert.Equal(text.Length - text.TrimStart().Length, c.StringLength);
@@ -25,7 +25,7 @@ namespace SimpleParser.Tests
         [Theory]
         public void T02_Should_parse_one_or_more(string text)
         {
-            var t = WhitecharsTokenizer.OneOrMoreWhitechars;
+            var t = WhitecharsTokenizer.OneOrMore;
             var c = t.Parse(text);
             Assert.NotNull(c);
             Assert.Equal(text.Length - text.TrimStart().Length, c.StringLength);
@@ -37,11 +37,53 @@ namespace SimpleParser.Tests
         [Fact]
         public void T03_Should_not_parse_one_or_more()
         {
-            var t = WhitecharsTokenizer.OneOrMoreWhitechars;
+            var t = WhitecharsTokenizer.OneOrMore;
             var c = t.Parse("");
             Assert.Null(c);
             c = t.Parse("XYZ");
             Assert.Null(c);
         }
+        
+        
+        [InlineData("")]
+        [InlineData("  ")]
+        [Theory]
+        public void T04_Should_parse_all0(string text)
+        {
+            var t = WhitecharsTokenizer.AllOrNone;
+            var c = t.Parse(text);
+            Assert.NotNull(c);
+            Assert.Equal(text.Length, c.StringLength);
+
+            c = t.Parse(text + "a");
+            Assert.Null(c);
+        }
+        
+        [Fact]
+        public void T05_Should_parse_all1()
+        {
+            const string text = "   ";
+            var          t    = WhitecharsTokenizer.All;
+            var          c    = t.Parse(text);
+            Assert.NotNull(c);
+            Assert.Equal(text.Length, c.StringLength);
+
+            c = t.Parse(text + "a");
+            Assert.Null(c);
+        }
+        
+                
+        [Fact]
+        public void T06_Should_not_parse_all1()
+        {
+            const string text = "";
+            var          t    = WhitecharsTokenizer.All;
+            var          c    = t.Parse(text);
+            Assert.Null(c);
+
+            c = t.Parse(text + "a");
+            Assert.Null(c);
+        }
     }
+    
 }
--- app/SimpleParser/iSukces.Parsers/TokenParsers/WhitecharsTokenizer.cs
+++ app/SimpleParser/iSukces.Parsers/TokenParsers/WhitecharsTokenizer.cs
@@ -4,21 +4,23 @@ namespace iSukces.Parsers.TokenParsers
 {
     public sealed class WhitecharsTokenizer : ValueTokenizer
     {
-        private WhitecharsTokenizer(bool mustHaveAtLeastOne)
+        private WhitecharsTokenizer(WhitecharsTokenizerKind kind)
         {
-            _mustHaveAtLeastOne = mustHaveAtLeastOne;
+            _zeroIsNotAcceptable = kind == WhitecharsTokenizerKind.All
+                                   || kind == WhitecharsTokenizerKind.AtLeastOne;
+            _whole = kind == WhitecharsTokenizerKind.AllOrNone || kind == WhitecharsTokenizerKind.All;
         }
 
 
         public override TokenCandidate Parse(string text)
         {
-            if (string.IsNullOrEmpty(text))
-                return CreateTokenCandidate(0);
+            if (string.IsNullOrEmpty(text)) return CreateTokenCandidate(0);
+
             for (var i = 0; i < text.Length; i++)
             {
                 if (char.IsWhiteSpace(text[i]))
                     continue;
-                return CreateTokenCandidate(i);
+                return _whole ? null : CreateTokenCandidate(i);
             }
 
             return CreateTokenCandidate(text.Length);
@@ -27,19 +29,73 @@ namespace iSukces.Parsers.TokenParsers
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private TokenCandidate CreateTokenCandidate(int length)
         {
-            if (_mustHaveAtLeastOne && length == 0)
+            var isZeroLength = length == 0;
+            if (_zeroIsNotAcceptable && isZeroLength)
                 return null;
-            return new TokenCandidate(null, length, length == 0 ? 0 : int.MaxValue);
+            return new TokenCandidate(null, length, isZeroLength ? 0 : int.MaxValue);
         }
 
-        public static WhitecharsTokenizer ZeroOrMoreWhitechars => InstanceHolder.MyZeroOrMoreWhitechars;
-        public static WhitecharsTokenizer OneOrMoreWhitechars  => InstanceHolder.MyOneOrMoreWhitechars;
-        private readonly bool _mustHaveAtLeastOne;
+        /// <summary>
+        ///  Texts starts from zero or any number of white chars 
+        /// </summary>
+        public static WhitecharsTokenizer ZeroOrMore => InstanceHolder.MyZeroOrMore;
+        
+        
+        /// <summary>
+        /// Texts starts from at least one white char
+        /// </summary>
+        public static WhitecharsTokenizer OneOrMore  => InstanceHolder.MyOneOrMore;
+        
+        /// <summary>
+        /// Text contains only white chars or is empty
+        /// </summary>
+        public static WhitecharsTokenizer AllOrNone  => InstanceHolder.MyAllOrNone;
+        
+        /// <summary>
+        /// Text contains only white chars
+        /// </summary>
+        public static WhitecharsTokenizer All        => InstanceHolder.MyAll;
+
+
+        private readonly bool _zeroIsNotAcceptable;
+        private readonly bool _whole;
 
         private sealed class InstanceHolder
         {
-            public static readonly WhitecharsTokenizer MyZeroOrMoreWhitechars = new WhitecharsTokenizer(false);
-            public static readonly WhitecharsTokenizer MyOneOrMoreWhitechars = new WhitecharsTokenizer(true);
+            public static readonly WhitecharsTokenizer MyZeroOrMore
+                = new WhitecharsTokenizer(WhitecharsTokenizerKind.ZeroOrMore);
+
+            public static readonly WhitecharsTokenizer MyOneOrMore =
+                new WhitecharsTokenizer(WhitecharsTokenizerKind.AtLeastOne);
+
+            public static readonly WhitecharsTokenizer MyAllOrNone =
+                new WhitecharsTokenizer(WhitecharsTokenizerKind.AllOrNone);
+
+            public static readonly WhitecharsTokenizer MyAll =
+                new WhitecharsTokenizer(WhitecharsTokenizerKind.All);
         }
+    }
+
+    public enum WhitecharsTokenizerKind
+    {
+        /// <summary>
+        /// Texts starts from zero or any number of white chars 
+        /// </summary>
+        ZeroOrMore,
+        
+        /// <summary>
+        /// Texts starts from at least one white char
+        /// </summary>
+        AtLeastOne,
+        
+        /// <summary>
+        /// Text contains only white chars or is empty
+        /// </summary>
+        AllOrNone,
+        
+        /// <summary>
+        /// Text contains only white chars
+        /// </summary>
+        All
     }
 }
